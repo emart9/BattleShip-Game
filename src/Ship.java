11fbde586738c0095edb5ec1ead697b002094e78
Alex Guler <aguler3@uic.edu>
2017-11-16 20:35:32 -0600

    out of bounds check and limited number of times you can put the ships

--- src/Ship.java
+++ src/Ship.java
@@ -99,5 +99,4 @@ public class Ship extends JButton
         return name;
     }
 
-
 }
--- src/userInteraction.java
+++ src/userInteraction.java
@@ -39,61 +39,56 @@ public class userInteraction
 
                         if (shipClicked != null)
                         {
-
-                            /*
-                            try { //adds image to grid buttons
-                            Image img1 = ImageIO.read(getClass().getResource("batt100.gif"));
-                            fired[row][col].setIcon(new ImageIcon(img1));
-                            fired[row][col].setHorizontalTextPosition(SwingConstants.CENTER);
-                            shipPositions[row][col].setIcon(new ImageIcon(img1));
-                            shipPositions[row][col].setHorizontalTextPosition(SwingConstants.CENTER);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                             */
-
-
                             // get image and if its vertical or horizontal
                             Image[] shipImage = shipClicked.getShipImage();
                             int r = B.getRow() - 1;
                             int c = B.getCol() - 1;
 
-                            // TODO: out of bounds check
+                            // if the ship that was clicked is in the horizontal state
                             if (shipClicked.isHorizontal())
                             {
-                                for (int i = 0; i < shipImage.length; ++i)
+                                // if the ship is within bounds place it.
+                                if (((c + shipImage.length)-1) < 10)
                                 {
-                                    playerGrid[r][c].setIcon(new ImageIcon(shipImage[i]));
-                                    c++;
+                                    for (int i = 0; i < shipImage.length; ++i)
+                                    {
+                                        playerGrid[r][c].setIcon(new ImageIcon(shipImage[i]));
+                                        c++;
+                                    }
+                                    shipClicked.setVisible(false);  // cannot put that ship on the board anymore
                                 }
-                                // System.out.println("HORIZONTAL: Gonna put this image from Column: " + c + " to " + (c + shipImage.length) + " in Row: " + r);
+                                // otherwise if it isn't, send a message that it is out of bounds
+                                else
+                                {
+                                    JOptionPane.showMessageDialog(null,
+                                            "Cannot place ship out of bounds.",
+                                            "Out of bounds!", JOptionPane.ERROR_MESSAGE);
+                                }
                             }
-                            // TODO: out of bounds check
+                            // otherwise if its in the vertical state
                             else
                             {
-                                for (int i = 0; i < shipImage.length; ++i)
+                                System.out.println("VERTICAL: Gonna put this image from Row: " + r + " to " + ((r + shipImage.length)-1) + " in Column: " + r);
+
+                                // if the ship is within bounds place it.
+                                if (((r + shipImage.length)-1) < 10)
                                 {
-                                    playerGrid[r][c].setIcon(new ImageIcon(shipImage[i]));
-                                    r++;
+                                    for (int i = 0; i < shipImage.length; ++i)
+                                    {
+                                        playerGrid[r][c].setIcon(new ImageIcon(shipImage[i]));
+                                        r++;
+                                    }
+                                    shipClicked.setVisible(false);  // cannot put that ship on the board anymore
                                 }
-                                // System.out.println("VERTICAL: Gonna put this image from Row: " + r + " to " + (r + shipImage.length) + " in Column: " + r);
+                                // otherwise if it isn't, send a message that it is out of bounds
+                                else
+                                {
+                                    JOptionPane.showMessageDialog(null,
+                                            "Cannot place ship out of bounds.",
+                                            "Out of bounds!", JOptionPane.ERROR_MESSAGE);
+                                }
                             }
                         }
-                        /*
-                         // TODO DELETE THIS LATER
-                        if (shipClicked != null)
-                        {
-                            System.out.println(shipClicked.getShipName() + " was clicked before this button!");
-                            System.out.println("Row: " + B.getRow() + " | Column: " + B.getCol());
-                        }
-                        else
-                        {
-                            System.out.println("No button was clicked before this button");
-                            System.out.println("Row: " + B.getRow() + " | Column: " + B.getCol());
-                        }
-                        */
-
-
                         shipClicked = null;
                     }
                 });
